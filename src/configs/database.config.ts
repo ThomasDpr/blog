538d568c979e33a8044f41879739f4895d104850
jboucly <julien.boucly@outlook.com>
2023-07-05 19:34:53 +0200

    core(typeorm): add migration script and configs

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -1,4 +1,6 @@
 import { registerAs } from '@nestjs/config';
+import { join } from 'path';
+import { DataSource } from 'typeorm';
 
 export default registerAs('database', () => ({
   sslActive: process.env.BDD_SSL === 'true',
@@ -10,3 +12,15 @@ export default registerAs('database', () => ({
   username: process.env.BDD_USERNAME || 'root',
   database: process.env.BDD_DATABASE || 'blog',
 }));
+
+export const connectionSource = new DataSource({
+  type: 'postgres',
+  synchronize: false,
+  host: process.env.BDD_HOST || '127.0.0.1',
+  password: process.env.BDD_PASSWORD || 'root',
+  username: process.env.BDD_USERNAME || 'root',
+  database: process.env.BDD_DATABASE || 'blog',
+  port: parseInt(process.env.BDD_PORT, 10) || 5455,
+  migrations: [join(__dirname, '..', 'migrations', '*.ts')],
+  entities: [join(__dirname, '..', '**', '*.entity{.ts,.js}')],
+});
